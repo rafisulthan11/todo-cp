5b4728decf429a06488737c2452d6d677c5d6970
Your Name <rakadaneshwara@gmail.com>
2025-05-27 14:51:17 +0700

    first commit

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    public function insert() {
+        $item_name = request()->get('item', 'Empty');
+
+        $task = new Task();
+        $task->name = $item_name;
+        $task->save();
+
+        return view('welcome');
+    }
+
+    public function delete($id) {
+        $to_delete = Task::where('id', $id)->firstOrFail();
+        $to_delete->delete();
+
+        return view('welcome');
+    }
+}
